9d1c2ad52b286b1f7dd49e01933dcafefe15ad76
Marilokms <42029758+Marilok@users.noreply.github.com>
2026-02-19 13:26:45 -0500

    security(website): remove comments

--- apps/website/next.config.ts
+++ apps/website/next.config.ts
@@ -19,11 +19,6 @@ const nextConfig: NextConfig = {
             key: "Cross-Origin-Opener-Policy",
             value: "same-origin",
           },
-          // {
-          //   key: "Content-Security-Policy",
-          //   value:
-          //     "default-src 'self'; script-src 'self' 'unsafe-inline'; object-src 'none'; base-uri 'self'; frame-ancestors 'none'; require-trusted-types-for 'script'",
-          // },
         ],
       },
     ];
