1025242a22a88096a135a1c33175f4eaff0c196f
Marilokms <42029758+Marilok@users.noreply.github.com>
2026-02-20 15:01:50 -0500

    fix(mantine-next): publish compiled dist exports for Node runtime

--- packages/mantine-next/src/index.ts
+++ packages/mantine-next/src/index.ts
@@ -1,6 +1,6 @@
-export { AnchorLink, type AnchorLinkProps } from "./AnchorLink";
-export { ActionIconLink, type ActionIconLinkProps } from "./ActionIconLink";
-export { ButtonLink, type ButtonLinkProps } from "./ButtonLink";
-export { LogoIcon, type LogoIconProps } from "./LogoIcon";
-export { ModalTitle, type ModalTitleProps } from "./ModalTitle";
-export { bonderyTheme, primaryColor } from "./theme";
+export { AnchorLink, type AnchorLinkProps } from "./AnchorLink.js";
+export { ActionIconLink, type ActionIconLinkProps } from "./ActionIconLink.js";
+export { ButtonLink, type ButtonLinkProps } from "./ButtonLink.js";
+export { LogoIcon, type LogoIconProps } from "./LogoIcon.js";
+export { ModalTitle, type ModalTitleProps } from "./ModalTitle.js";
+export { bonderyTheme, primaryColor } from "./theme.js";
