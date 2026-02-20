f4ac5daa526feb57a9f5da201a9bdbb64bdd4e0f
Marilokms <42029758+Marilok@users.noreply.github.com>
2026-02-20 13:56:21 -0500

    REDIRECTS

--- packages/helpers/src/index.ts
+++ packages/helpers/src/index.ts
@@ -4,7 +4,9 @@ export {
   WEBAPP_ROUTES,
   GITHUB_REPO_URL,
   CHROME_EXTENSION_URL,
+  HELP_DOCS_URL,
   SUPPORT_EMAIL,
+  STATUS_PAGE_URL,
   SOCIAL_LINKS,
 } from "./globals/paths.js";
 export { IMPORTANT_EVENT_TYPE_META } from "./globals/important-events.js";
